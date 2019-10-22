11ed6c44395490a28418fbe43a082b75a6785bfc
prateekchandrajha <prateekchandrajha@gmail.com>
2019-10-23 00:07:04 +0530

    Update bitbybit.py

--- bitbybit.py
+++ bitbybit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 21 17:59:58 2019
+Created on Mon Oct 22 17:59:58 2019
 
 @author: Bidhan
 """
@@ -11,19 +11,20 @@ data=[]
 data_extract=[]
 operate={}
 
-fin = open ('friday.txt', 'r')
-fout = open ('friday.out', 'w')
+#fin = open ('friday.txt', 'r')
+#fout = open ('friday.out', 'w')
 p=0
 
-for line in fin:
+"""for line in fin:
     data.append(line.strip("\n"))
     #print(data[p])
-    #p+=1
+    #p+=1"""
+    
+for line in sys.stdin:
+    data.append(line.rstrip())
     
 #x=0
 N=int(data[0])
-
-final_output=[]
 
 for j in range(32):
     operate[j]="?"
@@ -53,7 +54,6 @@ def AND(i,j):
     if (i==1 and j==1):
         return 1    
     
-    
 def CLEAR(i):
     return(0)
     
@@ -62,17 +62,20 @@ def SET(i):
 
 int_entries=[]
 for x in range(len(data)):
-    if(len(data[x])<=2):
+    if(len(data[x])<=3):
         int_entries.append(x)
-    
+ 
 final_print=""
+final_output=[]
+
     #N=int(data[x])
+    
 for i in range(len(int_entries)):
     N=int_entries[i]
     #if(type(int(data[N+1]))!=int):
     for j in range(int(data[N])):
         data_extract = data[N+j+1].split(" ")
-        print(data_extract)
+        #print(data_extract)
         if(data_extract[0]=="OR"):
             operate[int(data_extract[1])] = OR(operate[int(data_extract[1])],operate[int(data_extract[2])])
         if(data_extract[0]=="AND"):
@@ -81,17 +84,31 @@ for i in range(len(int_entries)):
             operate[int(data_extract[1])] = SET(operate[int(data_extract[1])])
         if(data_extract[0]=="CLEAR"):
             operate[int(data_extract[1])] = CLEAR(operate[int(data_extract[1])])
-       
+        
     for k in range(32):
         if int(data[N])!=0:
             #fout.write(str(operate[31-k]))
+            #print(str(operate[31-k]))
             final_print = final_print+str(operate[31-k])
             #print(k)
             #print(operate[31-k])
             if k==31:
+                #fout.write("\n")
                 final_output.append(final_print)
-                final_print=[]
+                final_print=""
+                """if i!=len(int_entries)-1:
+                    if int(data[int_entries[i+1]])!=0:
+                        print("\n")"""
+                for c in range(32):
+                    operate[31-c]="?"
+                    
+for k in range(len(final_output)):
+    print(final_output[k])
+    if(k!=(len(final_output)-1)):
+        print("\n")
                 
+            
+                    
         
                     
 
